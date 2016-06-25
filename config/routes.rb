540e8509b9cde17ca7d18d59d27e644992258301
Josh <jitzoe@gmail.com>
2016-06-25 16:01:57 -0400

    Cleaned up the pins scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  resources :pins
   devise_for :users
   root "pages#home"
   get "about" => "pages#about" # creates about_path
-  get "team" => "pages#team" # creates team_path
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
